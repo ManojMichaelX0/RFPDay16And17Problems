57630eb96ff4b9f7ecd65a68b715d4961abe171a
ManojMichaelX0 <manojthiparapu@gmail.com>
2022-03-16 23:01:03 +0530

    added Day 16 and 17 Practice Problems

---
+++ Day16And17Problems/Calender.cs
@@ -0,0 +1,295 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Day16And17Problems
+//{
+//    internal class Calender
+//    {
+//        public static bool isLeapYear(int year)
+//        {
+//            if (year % 4 == 0 || year % 400 == 0 && year % 100 != 0)
+//                return true;
+//            else
+//                return false;
+//        }
+//        public static int calculateDayOfWeek(int day, int month, int year)
+//        {
+//            int y1, x, m, d1;
+//            y1 = year - (14 - month) / 12;
+//            x = y1 + (y1 / 4) - (y1 / 100) + (y1 / 400);
+//            m = month + 12 * ((14 - month) / 12) - 2;
+//            d1 = (day + x + 31 * m / 12) % 7;
+//            return d1;
+//        }
+//        public static void printCalendar(int day, int days)
+//        {
+
+//            String[,] cal = new String[6, 7];
+//            int temp;
+//            String weekdaystr = "SUN MON TUE WED THU FRI SAT";
+//            Console.WriteLine(weekdaystr);
+//            switch (day)
+//            {
+//                case 0:
+//                    temp = 1;
+//                    for (int i = 0; i < cal.Length; i++)
+//                    {
+//                        for (int j = 0; j < cal[i].length; j++)
+//                        {
+//                            if (temp <= 9)
+//                            {
+//                                cal[i, j] = temp + "   ";
+//                            }
+//                            else
+//                            {
+//                                cal[i, j] = temp + "  ";
+//                            }
+//                            temp++;
+//                            if (temp == days + 1)
+//                            {
+//                                break;
+//                            }
+//                        }
+//                        if (temp == days + 1)
+//                        {
+//                            break;
+//                        }
+//                    }
+//                    break;
+//                case 1:
+//                    temp = 1;
+
+//                    for (int i = 0; i < cal.Length; i++)
+//                    {
+//                        for (int j = 0; j < cal[i].length; j++)
+//                        {
+//                            if (i == 0 && j == 0)
+//                            {
+//                                cal[i, j] = "    ";
+//                            }
+//                            else
+//                            {
+//                                if (i <= 9)
+//                                {
+//                                    cal[i, j] = temp + "   ";
+//                                }
+//                                else
+//                                {
+//                                    cal[i, j] = temp + "  ";
+//                                }
+//                                temp++;
+//                            }
+//                            if (temp == days + 1)
+//                            {
+//                                break;
+//                            }
+
+//                        }
+//                        if (temp == days + 1)
+//                        {
+//                            break;
+//                        }
+
+//                    }
+//                    break;
+//                case 2:
+//                    temp = 1;
+
+//                    for (int i = 0; i < cal.Length; i++)
+//                    {
+//                        for (int j = 0; j < cal[i].length; j++)
+//                        {
+//                            if (i == 0 && j == 0 || i == 0 && j == 1)
+//                            {
+//                                cal[i, j] = "    ";
+//                            }
+//                            else
+//                            {
+//                                if (temp <= 9)
+//                                {
+//                                    cal[i, j] = temp + "   ";
+//                                }
+//                                else
+//                                {
+//                                    cal[i, j] = temp + "  ";
+//                                }
+//                                temp++;
+//                            }
+//                            if (temp == days + 1)
+//                            {
+//                                break;
+//                            }
+
+//                        }
+//                        if (temp == days + 1)
+//                        {
+//                            break;
+//                        }
+
+//                    }
+//                    break;
+//                case 3:
+//                    temp = 1;
+
+//                    for (int i = 0; i < cal.Length; i++)
+//                    {
+//                        for (int j = 0; j < cal[i].length; j++)
+//                        {
+//                            if (i == 0 && j >= 0 || i == 0 && j <= 2)
+//                            {
+//                                cal[i, j] = "    ";
+//                            }
+//                            else
+//                            {
+//                                if (temp <= 9)
+//                                {
+//                                    cal[i, j] = temp + "   ";
+//                                }
+//                                else
+//                                {
+//                                    cal[i, j] = temp + "  ";
+//                                }
+//                                temp++;
+//                            }
+//                            if (temp == days + 1)
+//                            {
+//                                break;
+//                            }
+
+//                        }
+//                        if (temp == days + 1)
+//                        {
+//                            break;
+//                        }
+//                    }
+//                    break;
+//                case 4:
+
+//                    temp = 1;
+
+//                    for (int i = 0; i < cal.Length; i++)
+//                    {
+//                        for (int j = 0; j < cal[i].length; j++)
+//                        {
+//                            if (i == 0 && j >= 0 || i == 0 && j <= 3)
+//                            {
+//                                cal[i, j] = "    ";
+//                            }
+//                            else
+//                            {
+//                                if (temp <= 9)
+//                                {
+//                                    cal[i, j] = temp + "   ";
+//                                }
+//                                else
+//                                {
+//                                    cal[i, j] = temp + "  ";
+//                                }
+//                                temp++;
+//                            }
+//                            if (temp == days + 1)
+//                            {
+//                                break;
+//                            }
+
+//                        }
+//                        if (temp == days + 1)
+//                        {
+//                            break;
+//                        }
+//                    }
+//                    break;
+//                case 5:
+
+//                    temp = 1;
+
+//                    for (int i = 0; i < cal.Length; i++)
+//                    {
+//                        for (int j = 0; j < cal[i].length; j++)
+//                        {
+//                            if (i == 0 && j >= 0 || i == 0 && j <= 4)
+//                            {
+//                                cal[i, j] = "    ";
+//                            }
+//                            else
+//                            {
+//                                if (temp <= 9)
+//                                {
+//                                    cal[i, j] = temp + "   ";
+//                                }
+//                                else
+//                                {
+//                                    cal[i, j] = temp + "  ";
+//                                }
+//                                temp++;
+//                            }
+//                            if (temp == days + 1)
+//                            {
+//                                break;
+//                            }
+
+//                        }
+//                        if (temp == days + 1)
+//                        {
+//                            break;
+//                        }
+//                    }
+//                    break;
+//                case 6:
+
+//                    temp = 1;
+
+//                    for (int i = 0; i < cal.Length; i++)
+//                    {
+//                        for (int j = 0; j < cal[i].length; j++)
+//                        {
+//                            if (i == 0 && j >= 0 || i == 0 && j <= 5)
+//                            {
+//                                cal[i, j] = "    ";
+//                            }
+//                            else
+//                            {
+//                                if (temp <= 9)
+//                                {
+//                                    cal[i, j] = temp + "   ";
+//                                }
+//                                else
+//                                {
+//                                    cal[i, j] = temp + "  ";
+//                                }
+//                                temp++;
+//                            }
+//                            if (temp == days + 1)
+//                            {
+//                                break;
+//                            }
+
+//                        }
+//                        if (temp == days + 1)
+//                        {
+//                            break;
+//                        }
+//                    }
+//                    break;
+//            }
+//            for (int i = 0; i < cal.Length; i++)
+//            {
+//                for (int j = 0; j < cal[i].length; j++)
+//                {
+//                    if (cal[i, j] == null)
+//                        break;
+//                    Console.WriteLine(cal[i, j]);
+
+//                }
+//                if (cal[i] == null)
+//                    break;
+
+//            }
+
+//        }
+
+//    }
+//}
---
+++ Day16And17Problems/Program.cs
@@ -0,0 +1,373 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Collections;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Day16And17Problems
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Algorithm Programs \n1)  String Equal are not Program");
+            StringEquals se = new StringEquals();
+            string str = "MANOJ";
+            string str2 = "MANOJ";
+            string str3 = "manoj";
+            int n = str.Length;
+            int m = str2.Length;
+            int o = str3.Length;
+            StringEquals.permute(str, 0, n - 1);
+            Console.WriteLine("\n" + StringEquals.s + "\n");
+            StringEquals.s2 = StringEquals.s;
+            StringEquals.permute(str2, 0, m - 1);
+            Console.WriteLine("\n" + StringEquals.s);
+            StringEquals.s3 = StringEquals.s;
+            StringEquals.permute(str3, 0, o - 1);
+            Console.WriteLine("\n" + StringEquals.s + "\n");
+            StringEquals.s4 = StringEquals.s;
+            Console.WriteLine("Checking Lengths : True is if both strings are equal : Str and Str2 ");
+            Console.WriteLine(StringEquals.s2.Equals(StringEquals.s3));
+            Console.WriteLine("Checking Lengths : True is if both strings are equal : Str2 and Str3 ");
+            Console.WriteLine(StringEquals.s3.Equals(StringEquals.s4));
+            Console.WriteLine("-----------------------------------------------------------");
+
+            Console.WriteLine("\nAlgorithm Programs \n2) Binary Search Word Program\n");
+            BinarySearchWord bs = new BinarySearchWord();
+            bs.BinarySearch();
+
+            Console.WriteLine("\nAlgorithm Programs \n3) Sorted List Program\n");
+            SortedListProgram sp = new SortedListProgram();
+            sp.Sort();
+
+            Console.WriteLine("\nAlgorithm Programs \n4) Bubble Sort Program\n");
+            BubbleSorting bbs = new BubbleSorting();
+            bbs.BubbleSort();
+
+            Console.WriteLine("\nAlgorithm Programs \n5) Merge Sort \n");
+            MergeSortArray ms = new MergeSortArray();
+            ms.MergeSorting();
+
+            Console.WriteLine("\nAlgorithm Programs \n 6) Angrams or Not\n");
+            string firstString, secondString;
+
+            Console.WriteLine("\nEnter first string\n");
+            firstString = Console.ReadLine();
+            Console.WriteLine("\nEnter second string\n");
+            secondString = Console.ReadLine();
+            Anagrams a = new Anagrams();
+
+            if (a.areAnagram(firstString, secondString) == true)
+            {
+                Console.WriteLine("\nBoth string are anagram string\n");
+            }
+            else
+            {
+                Console.WriteLine("\nBoth string are not anagram string\n");
+            }
+
+            Console.WriteLine("\nAlgorithm Program \n7) Prime Number From 1 to 1000 Are :\n");
+            PrimeRange1_1000 p = new PrimeRange1_1000();
+            p.PrimeRange();
+
+            Console.WriteLine("\nAlgorithm Program \n8) Prime Palindromes From 1 to 1000 Are :\n");
+            PrimePalindromes pp = new PrimePalindromes();
+            pp.primePalindrome(1, 1000);
+
+            Console.WriteLine("\nAlgorithm Program \n9) Generic Search and Sort\n");
+            GenericSearchAndSort g = new GenericSearchAndSort();
+            g.BinarySearchGeneric<string>();
+
+            
+            Console.WriteLine("\nAlgorithm Programs \n10) Question Number\n");
+
+            QuestionNumber q = new QuestionNumber();
+            Console.WriteLine("Enter any Number :");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int range = (int)(Math.Pow(2, n));
+            Console.WriteLine("Take number between '0' to " + range);
+            int count = 0;
+            string input = null;
+            int lower = 0;
+            int upper = range;
+            int middle = (lower + upper / 2);
+            QuestionNumber.binarySearch(lower, upper, middle, count, input, number);
+
+
+            Console.WriteLine("Note : After Runing for 10 algorithm Question please Comment the 10 problem Code So that Next Lines of Code Will Execute");
+
+
+            Console.WriteLine("\nAlgorithms Program \n 12) Regex");
+            Console.WriteLine("Enter First Name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter Full Name");
+            string fullName = Console.ReadLine();
+            Console.WriteLine("Enter Contact Number");
+            string contactNumber = Console.ReadLine();
+            Console.WriteLine("Enter Date");
+            string date = Console.ReadLine();
+            RegexStringFunctions regex = new RegexStringFunctions();
+            regex.patternString(firstName, fullName, contactNumber, date);
+
+            Console.WriteLine("Data Structures \n1) Unordered List");
+            List<string> my_list = new List<string>();
+            my_list.Add("Program");
+            my_list.Add("Adding");
+            my_list.Add("Words");
+            my_list.Add("To");
+            my_list.Add("List");
+            my_list.Add("In");
+            my_list.Add("File");
+            Console.WriteLine("\nWriting Data into List");
+            File.WriteAllLines(@"D:\.net\Day16&17Problems\list.txt", my_list);
+            foreach (string words in my_list)
+            {
+                Console.WriteLine(words);
+            }
+            Console.WriteLine("Reading data");
+            File.ReadAllLines(@"D:\.net\Day16&17Problems\list.txt");
+            Console.WriteLine("\nEnter Word to Search\n");
+            string word = Console.ReadLine();
+            Console.WriteLine("\n");
+            bool value = my_list.Contains(word);
+            if (value == true)
+            {
+                my_list.Remove(word);
+                foreach (string ab in my_list)
+                {
+                    Console.WriteLine(ab);
+                }
+            }
+            else
+            {
+                my_list.Add(word);
+                foreach (string ab in my_list)
+                {
+                    Console.WriteLine(ab);
+                }
+            }
+
+            Console.WriteLine("Data Structure \n2) OrderedList");
+            int[] intArray = new int[5];
+            Console.WriteLine("Enter 5 Numbers ");
+            intArray[0] = Convert.ToInt32(Console.ReadLine());
+            intArray[1] = Convert.ToInt32(Console.ReadLine());
+            intArray[2] = Convert.ToInt32(Console.ReadLine());
+            intArray[3] = Convert.ToInt32(Console.ReadLine());
+            intArray[4] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Numbers in Asending Order");
+            int temp;
+            for (int j1 = 0; j1 <= intArray.Length - 2; j1++)
+            {
+                for (int i1 = 0; i1 <= intArray.Length - 2; i1++)
+                {
+                    if (intArray[i1] > intArray[i1 + 1])
+                    {
+                        temp = intArray[i1 + 1];
+                        intArray[i1 + 1] = intArray[i1];
+                        intArray[i1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Sorted:");
+            foreach (int p1 in intArray)
+                Console.Write(p1 + "\n");
+
+
+            Console.WriteLine("Data Structures \n3) Balanced Paranthasis");
+            Console.WriteLine("Enter max Size");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Stack stack = new Stack(n1);
+            Console.WriteLine("Enter expression: ");
+            string exp = Console.ReadLine();
+            int len = exp.Length;
+            Console.WriteLine("Matches and Mismatches: ");
+            for (int i2 = 0; i2 < len; i2++)
+            {
+                char ch = exp[i2];
+
+                if (ch == '(')
+                {
+                    stack.push(i2);
+                }
+                else if (ch == ')')
+                {
+                    try
+                    {
+                        long p1 = (stack.pop() + 1);
+                        Console.WriteLine("')' at index " + (i2 + 1) + " matched with '(' at index " + p1);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("')' at index " + (i2 + 1) + " is unmatched");
+                    }
+                }
+            }
+            while (!stack.isEmpty())
+            {
+                Console.WriteLine("'(' at index " + (stack.pop() + 1) + " is unmatched");
+            }
+
+            Console.WriteLine("Data Structure \n4) Bank Cash Counter");
+            char agn;
+            do
+            {
+                fuctions k1 = new fuctions();
+                int num;
+                Console.WriteLine("Please Select Any Function.");
+                Console.WriteLine("\nPress 1 for Deposit an Amount. \nPress 2 for Withdraw an Amount.");
+                num = Convert.ToInt32(Console.ReadLine());
+                switch (num)
+                {
+                    case 1:
+                        k1.fun1();
+                        break;
+                    case 2:
+                        k1.fun2();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Selection!!!");
+                        break;
+                }
+                Console.WriteLine("\nDo you want to continue this program? (y/n)");
+                agn = Convert.ToChar(Console.ReadLine());
+
+            } while (agn == 'y');
+
+            Console.ReadKey();
+
+            Console.WriteLine("Data Structure \n5) Palindrome Checker");
+            string s, revs = "";
+            Console.WriteLine(" Enter string");
+            s = Console.ReadLine();
+            for (int i3 = s.Length - 1; i3 >= 0; i3--)
+            {
+                revs += s[i3].ToString();
+            }
+            if (revs == s)
+            {
+                Console.WriteLine("String is Palindrome \n Entered String Was {0} and reverse string is {1}", s, revs);
+            }
+            else
+            {
+                Console.WriteLine("String is not Palindrome \n Entered String Was {0} and reverse string is {1}", s, revs);
+            }
+
+            Console.WriteLine("Data Structure \n8) Prime Range Array");
+            RangePrimeArray r = new RangePrimeArray();
+
+            Console.WriteLine("Enter the number of rows : ");
+
+            int m1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the number of columns : ");
+
+            int n2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+            int[,] A = new int[m1, n2];
+
+            int[] B = new int[m1 * n2];
+
+            int i = 0, j;
+
+            int k = 1;
+
+            while (i < m1 * n2)
+
+            {
+
+                if (r.isPrime(k) == true)
+
+                {
+
+                    B[i] = k;
+
+                    i++;
+
+                }
+
+                k++;
+
+            }
+
+            int x = 0;
+
+            for (i = 0; i < m1; i++)
+
+            {
+
+                for (j = 0; j < n2; j++)
+
+                {
+
+                    A[i, j] = B[x];
+
+                    x++;
+
+                }
+
+            }
+
+            Console.WriteLine("The Final Array is : ");
+
+            for (i = 0; i < m1; i++)
+
+            {
+
+                for (j = 0; j < n2; j++)
+
+                {
+
+                    Console.WriteLine(A[i, j] + "\t");
+
+                }
+
+            }
+            //Console.WriteLine("\nData Structres \n12) Calneder");
+            
+            //int year, month;
+            //bool isLeapYear;
+            //int days;
+            //int day;
+            //int[] daysPerYear = { 31, 28, 30, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            //Console.WriteLine("Enter month");
+            //month = Convert.ToInt32(Console.ReadLine());
+            //if (month > 0 && month <= 12)
+            //{
+            //    Console.WriteLine("Enter year");
+            //    year = Convert.ToInt32(Console.ReadLine());
+            //    if (year > 1582 && year < 9999)
+            //    {
+            //        isLeapYear = Calender.isLeapYear(year);
+            //        if (isLeapYear && month == 2)
+            //        {
+            //            days = 29;
+            //            day = Calender.calculateDayOfWeek(1, month, year);
+            //            Calender.printCalendar(day, days);
+
+            //        }
+            //        else
+            //        {
+            //            days = daysPerYear[month - 1];
+            //            day = Calender.calculateDayOfWeek(1, month, year);
+            //            Calender.printCalendar(day, days);
+            //        }
+            //    }
+            //    else
+            //        Console.WriteLine("please enter valid year");
+            //}
+            //else
+            //    Console.WriteLine("Please enter valid month");
+
+            
+        }
+    }
+}
+
